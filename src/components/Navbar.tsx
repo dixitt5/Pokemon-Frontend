a239f2b75886beeebbac8ea6018fc52ee271251d
Dixit Tilaji <tilajidixit@gmail.com>
2024-02-13 16:27:31 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = ({ flag }: { flag: boolean }): ReactElement => {
   const navigate = useNavigate()
   return (
     <div className="flex flex-row justify-between font-semibold dark:bg-green-800 dark:text-white bg-blue-400 text-black p-4 items-center">
-      <h1 onClick={() => { navigate('/pokemons') } } className="text-4xl mx-3 cursor-pointer">Pokemons {flag ? 'Page' : 'Details'}</h1>
+      <h1 onClick={() => { navigate('/pokemons') } } className="text-4xl mx-3 cursor-pointer">Pokemons on {flag ? 'Page' : 'Details'}</h1>
       <div className='flex flex-row items-center gap-4 p-4  '>
         <div className='text-xl cursor-pointer hover:underline' onClick={() => { navigate('search') }}>Search</div>
         <DarkModeToggle/></div>
